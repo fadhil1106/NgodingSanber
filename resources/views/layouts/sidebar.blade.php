45482676c7b2fab59aae34a778d954d9ef60b81d
Fuad Fadhil Azzar <30482903+fadhil1106@users.noreply.github.com>
2020-07-09 10:07:29 +0800

    edit layout

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,9 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="#" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">NgodingSanber</span>
     </a>
 
     <!-- Sidebar -->
@@ -26,40 +24,29 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview menu-open">
+          <li class="nav-item">
             <a href="#" class="nav-link active">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <i class="nav-icon fas fa-home"></i>
               <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
+                Home
               </p>
             </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="./index.html" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
-                </a>
-              </li>
-            </ul>
           </li>
           <li class="nav-item">
-            <a href="pages/widgets.html" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-plus-square"></i>
               <p>
-                Widgets
-                <span class="right badge badge-danger">New</span>
+                Buat Pertanyaan
               </p>
             </a>
           </li>
           
-          <li class="nav-header">EXAMPLES</li>
+          <li class="nav-header">Halaman Pribadi</li>
           <li class="nav-item">
-            <a href="pages/calendar.html" class="nav-link">
-              <i class="nav-icon far fa-calendar-alt"></i>
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-list"></i>
               <p>
-                Calendar
-                <span class="badge badge-info right">2</span>
+                Pertanyaan Anda
               </p>
             </a>
           </li>
