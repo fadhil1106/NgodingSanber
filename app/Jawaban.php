f52a929a5f36501802c331ce2a88667cbec86d0a
Fuad Fadhil Azzar <30482903+fadhil1106@users.noreply.github.com>
2020-07-10 17:16:57 +0800

    add relation in model jawaban

--- app/Jawaban.php
+++ app/Jawaban.php
@@ -12,4 +12,9 @@ class Jawaban extends Model
     {
         return $this->belongsTo('App\Pertanyaan');
     }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
