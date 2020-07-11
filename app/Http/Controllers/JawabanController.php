643c4b64bae1246e614f5aea142cad3b664ae50b
Fuad Fadhil Azzar <30482903+fadhil1106@users.noreply.github.com>
2020-07-12 00:26:07 +0800

    add store jawaban

---
+++ app/Http/Controllers/JawabanController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jawaban;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+
+class JawabanController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'jawaban' => 'required|min:10'
+        ]);
+        if ($validator->fails()) {
+            return back()
+                    ->withErrors($validator)
+                    ->withInput();
+        }
+        Jawaban::create($request->all());
+        return back();
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
